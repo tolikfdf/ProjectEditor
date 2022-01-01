c333b75c295ff67c4419c81a50a939265b842862
Tolegen Aitmambetov <tolikfdf@rambler.ru>
2022-01-02 01:55:47 +0600

    Revert "delete comments to see how github works"
    This reverts commit 4457d5b824de03e00b076f084522ada54b9a20d0.

--- ProjectEditor/Program.cs
+++ ProjectEditor/Program.cs
@@ -5,6 +5,7 @@ using Contracts;
 var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 
+// Add services to the container.
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureLoggerService();
 
@@ -12,11 +13,13 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureLoggerService();
 builder.Services.AddControllers().AddNewtonsoftJson();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
