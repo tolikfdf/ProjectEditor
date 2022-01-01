4457d5b824de03e00b076f084522ada54b9a20d0
Tolegen Aitmambetov <tolikfdf@rambler.ru>
2022-01-02 01:52:27 +0600

    delete comments to see how github works

--- ProjectEditor/Program.cs
+++ ProjectEditor/Program.cs
@@ -5,7 +5,6 @@ using Contracts;
 var builder = WebApplication.CreateBuilder(args);
 LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 
-// Add services to the container.
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureLoggerService();
 
@@ -13,13 +12,11 @@ builder.Services.ConfigureRepositoryManager();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.ConfigureLoggerService();
 builder.Services.AddControllers().AddNewtonsoftJson();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
